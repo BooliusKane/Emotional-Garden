5747f2f8fc589638d6ab6378125a40674d49a3ba
Isabelle/nyanroki <isablin2005@gmail.com>
2025-05-26 14:28:05 -0700

    Added under construction img

--- loading_screen/js/main.js
+++ loading_screen/js/main.js
@@ -3,13 +3,14 @@
     JavaScript - Grubby Loading Screen
     May 2025
 */
-
+    
 // Array of image URLs - Replace with your image file names
-const images = [
-   img.src = './img/unpaidgrub.png',
-   img.src = './img/barrowgrub.png',
-   img.src = './img/sleepygrubsky.png',
-   img.src = './img/leafgrub.png',
-   img.src = './img/watergrub.png',
-   img.src = './img/pupasoilgrub.png',
+ const loadingImages = [
+    'img/unpaidgrub.png',
+    'img/barrowgrub.png',
+    'img/sleepygrubsky.png',
+    'img/leafgrub.png',
+    'img/watergrub.png',
+    'img/pupasoilgrub.png',
 ];
+
