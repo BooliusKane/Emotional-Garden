9fd59e11c2b202de996d5f9c5304c3717d8f2cc6
Isabelle/nyanroki <isablin2005@gmail.com>
2025-05-26 13:01:49 -0700

    Ok so i fucked up the loading screen so I deleted everything to restart

--- loading_screen/js/main.js
+++ loading_screen/js/main.js
@@ -14,16 +14,3 @@ const images = [
    img.src = './img/watergrub.png',
    img.src = './img/pupasoilgrub.png',
 ];
-
-// Function to set a random background image
-function setRandomBackgroundImage() {
-   // Get a random index from the array of images
-   const randomIndex = Math.floor(Math.random() * images.length);
-
-   // Set the selected image as a CSS background-image for the body element
-      document.body.style.backgroundImage=`url('${images[randomIndex]}')`;
-}
-
-// Call the function to initialize a random background image when the page loads
-
-$("#output").html(setRandomBackgroundImage());
