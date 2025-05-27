fbc440799a2826b7f0474a4efa0d238161399bf3
daisyl68 <dly11@ucsc.edu>
2025-05-27 01:55:34 -0700

    Back Button and Grubby Folder Edits

--- js/main.js
+++ js/main.js
@@ -1,6 +1,36 @@
 // main.js - handles global clickable assets like Chia and greenhosue
 // Used by: index.html (main page)
 
+// LOADING SCREEN
+// Array of image URLs - Replace with your image file names
+let randomNumber = Math.floor(Math.random() * 6) + 1;
+
+function load(randomNumber) {
+    let img;
+    if (randomNumber === 1) {
+        img = document.createElement('img');
+        img.src = './img/leafgrub.png';
+    } else if (randomNumber === 2) {
+        img = document.createElement('img');
+        img.src = './img/sleepygrubsky.png';
+    } else if (randomNumber === 3) {
+        img = document.createElement('img');
+        img.src = './img/pupasoilgrub.png';
+    } else if (randomNumber === 4) {
+        img = document.createElement('img');
+        img.src = './img/barrowgrub.png';
+    } else if (randomNumber === 5) {
+        img = document.createElement('img');
+        img.src = './img/watergrub.png';
+    } else if (randomNumber === 6) {
+        img = document.createElement('img');
+        img.src = './img/unpaidgrub.png';
+    }
+   return img;
+}
+let loadingscreen = load(randomNumber);
+ 
+
 $(document).ready(function() {
    let isAwake = false;
 
