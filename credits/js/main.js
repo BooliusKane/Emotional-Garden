87b46b7be919c47db12f047e7f530f216daff01c
daisyl68 <dly11@ucsc.edu>
2025-06-05 17:59:32 -0700

    Credits and Help Wanted Edits

--- credits/js/main.js
+++ credits/js/main.js
@@ -17,3 +17,20 @@ document.addEventListener('DOMContentLoaded', function() {
         // constructionImage.classList.toggle('visible');
     });
 });
+
+var expandDetailsButton = document.getElementById("expand-details");
+var myTitle = document.getElementById("myTitle");
+var myList = document.getElementById("myList");
+
+expandDetailsButton.click(function(){
+    expand-details.classList.toggle("active");
+    // Append a new div to our output div
+    $("#output").html()
+});
+
+
+
+$("#reveal").click(function(){
+    // Append a new div to our output div
+    $("#output1").html('<div class="text1"><p>' + "Ajax AKA Childe AKA Tartaglia (Genshin Impact)" + '</p></div>');
+});
